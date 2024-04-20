67414347b3959ea98c9729d5b3a42d411be86fff
33tm <80350771+33tm@users.noreply.github.com>
2024-04-20 16:09:33 -0700

    update

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, json } from "express"
 import { Namespace, Server, Socket } from "socket.io"
 import cors from "cors"
 
+import { dirname } from "path"
 import { readdirSync, statSync } from "fs"
 import { createServer } from "http"
 
@@ -19,20 +20,19 @@ const config = { origin: ["http://localhost:3000"] }
 
 const rest = express()
 const server = createServer(rest)
-const socket = new Server(server, { cors: config })
+const io = new Server(server, { cors: config })
 
 rest.use(json(), cors(config))
 
 if (process.env.NODE_ENV === "production") {
     import("@socket.io/cluster-adapter")
-        .then(({ createAdapter }) => socket.adapter(createAdapter()))
+        .then(({ createAdapter }) => io.adapter(createAdapter()))
     import("@socket.io/sticky")
-        .then(({ setupWorker }) => setupWorker(socket))
+        .then(({ setupWorker }) => setupWorker(io))
 }
 
 export type Route = (req: Request, res: Response) => void
 export type Handler = (server: Namespace, socket: Socket, data: unknown) => void
-type Event = [string, Handler]
 
 const importRoutes = (root: string) => {
     readdirSync(root).forEach(file => {
@@ -55,46 +55,34 @@ const importRoutes = (root: string) => {
 
 importRoutes("src/rest")
 
-const namespaces: [string, Event[]][] = []
-
-const importEvents = async (root: string) => {
-    const events: Event[] = []
-    await Promise.all(readdirSync(root).map(async file => {
+const events = async (root: string) => {
+    readdirSync(root).forEach(file => {
         const path = `${root}/${file}`
-        if (statSync(path).isDirectory()) return importEvents(path)
+        if (statSync(path).isDirectory()) return events(path)
         if (!file.endsWith(".ts")) return
-        await import(`./${path.slice(4, -3)}`).then(({ handler }) => {
+        console.log(file)
+        import(`./${path.slice(4, -3)}`).then(({ handler }: { handler: Handler }) => {
             if (!handler) return
-            events.push([file.slice(0, -3), handler])
-        })
-    }))
-    if (events.length) {
-        const namespace = root.slice(10) || "/"
-        namespaces.push([namespace, events])
-        console.log(`Socket ${namespace} [${events.map(i => i[0]).join(", ")}]`)
-    }
-}
-
-importEvents("src/socket").then(() => {
-    namespaces.forEach(([namespace, events]) => {
-        const ns = socket.of(namespace)
-        ns.on("connection", connection => {
-            events.forEach(([event, handler]) => {
-                connection.on(event, data => {
-                    handler(ns, connection, data)
+            const namespace = io.of(`/${dirname(path).slice(11)}`)
+            namespace.on("connection", socket => {
+                socket.on(file.slice(0, -3), data => {
+                    handler(namespace, socket, data)
                 })
             })
+            console.log(`Socket ${namespace.name} [${file.slice(0, -3)}]`)
         })
     })
-})
+}
 
-socket.of("/auth").on("connection", connection => {
-    connection.on("disconnect", () => {
-        tokens.delete(connection.id)
+events("src/socket")
+
+io.of("/auth").on("connection", socket => {
+    socket.on("disconnect", () => {
+        tokens.delete(socket.id)
     })
 })
 
-socket.of("/app").use((socket, next) => {
+io.of("/app").use((socket, next) => {
     const { token } = socket.handshake.auth
     if (!token) next(Error())
     try {
